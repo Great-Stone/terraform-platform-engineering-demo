b11db563eabbc1eeef16e14f3649688e2f980db0
Great-Stone <hahohh@gmail.com>
2025-11-10 12:48:44 +0900

    Fix: Remove duplicate terraform block from registry/main.tf

--- registry/main.tf
+++ registry/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "~> 0.0"
-    }
-  }
-}
-
 provider "tfe" {
   hostname = var.tfe_hostname
   token    = var.tfe_token
