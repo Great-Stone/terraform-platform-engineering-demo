c26308500d0ff8360ff9d0b76752a7b212ee16b9
Great-Stone <hahohh@gmail.com>
2025-11-10 13:28:00 +0900

    Update no-code module configuration: add version_pin and document variables without defaults

--- registry/main.tf
+++ registry/main.tf
@@ -1,50 +1,84 @@
-provider "tfe" {
-  hostname = var.tfe_hostname
-  token    = var.tfe_token
-}
-
 # Base 3-tier 모듈 등록
 resource "tfe_registry_module" "base_3tier" {
   organization = var.tfe_organization
-  name         = "terraform-aws-base-3tier"
-  provider     = "aws"
-  namespace    = var.tfe_organization
 
   vcs_repo {
+    display_identifier = "terraform-aws-platform-demo-base-3tier"
     identifier         = var.github_repo_identifier
     branch             = var.github_branch
-    ingress_submodules = false
     oauth_token_id     = var.github_oauth_token_id
+    source_directory   = "modules/terraform-aws-base-3tier"
   }
+
+  initial_version = "0.0.0"
 }
 
 # S3 Add-on 모듈 등록
 resource "tfe_registry_module" "addon_s3" {
   organization = var.tfe_organization
-  name         = "terraform-aws-addon-s3"
-  provider     = "aws"
-  namespace    = var.tfe_organization
 
   vcs_repo {
+    display_identifier = "terraform-aws-platform-demo-addon-s3"
     identifier         = var.github_repo_identifier
     branch             = var.github_branch
-    ingress_submodules = false
     oauth_token_id     = var.github_oauth_token_id
+    source_directory   = "modules/terraform-aws-addon-s3"
   }
+
+  initial_version = "0.0.0"
 }
 
 # LB Add-on 모듈 등록
 resource "tfe_registry_module" "addon_lb" {
   organization = var.tfe_organization
-  name         = "terraform-aws-addon-lb"
-  provider     = "aws"
-  namespace    = var.tfe_organization
 
   vcs_repo {
+    display_identifier = "terraform-aws-platform-demo-addon-lb"
     identifier         = var.github_repo_identifier
     branch             = var.github_branch
-    ingress_submodules = false
     oauth_token_id     = var.github_oauth_token_id
+    source_directory   = "modules/terraform-aws-addon-lb"
   }
+
+  initial_version = "0.0.0"
+}
+
+# Base 3-tier No-Code 모듈
+# default가 없는 변수들: project_name, web_instance_type, was_instance_type, web_instance_count, was_instance_count, db_password
+resource "tfe_no_code_module" "base_3tier" {
+  organization    = var.tfe_organization
+  registry_module = tfe_registry_module.base_3tier.id
+  enabled         = true
+  version_pin     = "0.0.0"
+
+  variable_options {
+    name    = "web_instance_type"
+    type    = "string"
+    options = ["t3.micro", "t3.small", "t3.medium"]
+  }
+
+  variable_options {
+    name    = "was_instance_type"
+    type    = "string"
+    options = ["t3.micro", "t3.small", "t3.medium"]
+  }
+}
+
+# S3 Add-on No-Code 모듈
+# default가 없는 변수들: project_name, base_3tier_workspace_name, bucket_name
+resource "tfe_no_code_module" "addon_s3" {
+  organization    = var.tfe_organization
+  registry_module = tfe_registry_module.addon_s3.id
+  enabled         = true
+  version_pin     = "0.0.0"
+}
+
+# LB Add-on No-Code 모듈
+# default가 없는 변수들: project_name, base_3tier_workspace_name
+resource "tfe_no_code_module" "addon_lb" {
+  organization    = var.tfe_organization
+  registry_module = tfe_registry_module.addon_lb.id
+  enabled         = true
+  version_pin     = "0.0.0"
 }
 
